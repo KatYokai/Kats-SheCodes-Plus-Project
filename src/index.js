65d056a300b7306e8ff7f2c03c3900ffdb479bb5
KatYokai <krsalas@hotmail.com>
2023-08-07 18:25:21 -0400

    Changed back to old Weather Api

--- src/index.js
+++ src/index.js
@@ -16,3 +16,27 @@ let minutes = now.getMinutes();
 let date = document.querySelector("#date");
 date.innerHTML = `${day} ${hours}:${minutes}`;
 //City and Temp
+function location(event) {
+  event.preventDefault();
+  let input = document.querySelector("#formGroupExampleInput").value;
+  cityText.innerHTML = `${input.value}`;
+
+  locationTemp(input);
+}
+let cityText = document.querySelector("#city-text");
+let locationInput = document.querySelector("#location-input");
+locationInput.addEventListener("submit", location);
+
+function showTemp(response) {
+  let temp = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.main.temp);
+  temp.innerHTML = `${temperature}`;
+  cityText.innerHTML = response.data.name;
+}
+function locationTemp(input) {
+  let apiKey = "fda3688b1db05987dd5d07c237aecfba";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemp);
+}
+navigator.geolocation.getCurrentPosition(locationTemp);
