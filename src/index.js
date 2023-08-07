a6b5adf9bcabf57c3fa7356d8c65c4e3ac000952
KatYokai <krsalas@hotmail.com>
2023-08-07 18:24:28 -0400

    Revert "Change Weather Api"
    This reverts commit e827b403d642707e34c0de4bd628a2989e7d76d2.

--- src/index.js
+++ src/index.js
@@ -16,31 +16,3 @@ let minutes = now.getMinutes();
 let date = document.querySelector("#date");
 date.innerHTML = `${day} ${hours}:${minutes}`;
 //City and Temp
-function displayTemp(response) {
-  let tempElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city-text");
-  let condition = document.querySelector("#condition");
-  let humidity = document.querySelector("#humidity");
-  let wind = document.querySelector("#wind");
-  let humidityNumber = response.data.temperature.humidity;
-  let windNumber = Math.round(response.data.wind.speed);
-  tempElement.innerHTML = Math.round(response.data.temperature.current);
-  cityElement.innerHTML = response.data.city;
-  condition.innerHTML = response.data.condition.description;
-  humidity.innerHTML = `${humidityNumber}%`;
-  wind.innerHTML = `${windNumber}km/h`;
-}
-
-function showTemp(response) {
-  let temp = document.querySelector("#temperature");
-  let temperature = Math.round(response.data.temperature.day);
-  temp.innerHTML = `${temperature}`;
-  cityText.innerHTML = response.data.city;
-}
-function locationTemp(input) {
-  let apiKey = "79a5e8b38cf0f847t9136fa452o839aa";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${input.value}&key=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(showTemp);
-}
-navigator.geolocation.getCurrentPosition(locationTemp);
